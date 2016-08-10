1abf8d541b9b289b8dce63f1ad2f55b6e522050f
arturo <arturo@viu-turing.psych.ucsb.edu>
2016-08-10 16:35:01 -0700

    Finished computing foveal and peripheral masks .py

--- python/README.md
+++ python/README.md
@@ -1,1 +1,10 @@
 #Piranhas [Python 2.7]
+
+1. Download the Piranhas toolbox for python.
+2. Define your Computer + Human perception parameters.
+
+3. Create a Peripheral Architecture.
+
+4. Pool your dense feature maps:
+
+5. For a demo run from the MATLAB command line prompt:
--- python/create_Piranha.py
+++ python/create_Piranha.py
@@ -2,7 +2,10 @@
 # Developed by Arturo Deza as part of the Piranhas Toolkit
 # Questions & bugs: deza @ dyns.ucsb.edu
 
+from numpy import np
 from piranhas import *
+
+import math
 
 # Run Intialization Parameters:
 param = param_init_all()
@@ -13,35 +16,94 @@ e0_in_deg = param.e0_in_deg
 visual_field_radius_in_deg = param.visual_field_radius_in_deg
 deg_per_pixel = param.deg_per_pixel
 
-
 # Get Peripheral Architecture Parameters
+
+# Given the scale, compute the N_e and N_theta parameters.
+# You can also uncomment this line and input any values for N_e and N_theta.
+# Please refer to Freeman & Simoncelli, 2011 to check what values of scale
+# are biologically plausible for V1 and V2 receptive field sizes.
 
 N_param = get_pooling_parameters(scale,e0_in_deg,visual_field_radius_in_deg,deg_per_pixel)
 
 N_e = N_param[0]
 N_theta = N_param[1]
 
-# Create Regions:
-
-# Commenting this out since it went on the previous version
-#
-#e_max = visual_field_radius_in_deg
-#visual_field_width = round(2*(visual_field_radius_in_deg/deg_per_pixel))
-#
-#center_r = round(visual_field_width/2)
-#center_c = center_r
-#
-#regions = create_regions_vector_function_smooth(e0_in_deg,e_max,visual_field_width,deg_per_pixel,N_theta,N_e)
-
-##############################
-# Indexed Peripheral Regions #
-##############################
-
-visual = 1
-
-peripheral_regions = generate_pooling_regios_vector_smooth(deg_per_pixel,N_e,N_theta,visual_field_radius_in_deg,fovea,e0_in_deg,visual)
+#visual flag?
+visual = 0
 
 # The peripheral_regions data structure allows you to do anything you want with the pooling regions.
+peripheral_filters = generate_pooling_regios_vector_smooth(deg_per_pixel,N_e,N_theta,visual_field_radius_in_deg,fovea,e0_in_deg,visual)
+
+##################################
+# Compute Peripheral Mask Stream #
+##################################
 
 
+# size might have changed after removing peripheral regions inside fovea
+param_check = np.shape(peripheral_filters.regions)
+
+N_theta = param_check[0]
+N_e = param_check[1]
+peri_height = param_check[2]
+peri_width = param_check[3]
+
+select_mask_stream = np.empty((N_theta*N_e,peri_height,peri_width),dtype=object)
+select_mask_label = np.zeros((peri_height,peri_width))
+
+for z in range(1,N_theta*N_e):
+	i1 = np.mod(z,N_theta)
+	if i1==0:
+		i1 = N_theta
+	
+	j1 = np.round(np.floor((z-0.1)/N_theta)+1)
+	select_mask_stream[z-1,:,:] = np.squeeze(peripheral_filters.regions[i1-1,j1-1,:,:])
+	
+	select_loc = np.where(select_mask_stream[z-1,:,:]>0)
+	select_mask_label[select_loc[0],select_loc[1]] = z-1
+
+###################
+# Get Foveal Mask #
+###################
+
+dist_mask = np.empty(N_theta)
+for i in range(0,N_theta):
+	point_vector = np.empty(np.shape(peripheral_filters.offsets_r[i][0])[0])
+	for j in range(0,np.shape(peripheral_filters.offsets_r[i][0])[0]):
+		#Compute L2 norm (euclidean distance from center of fixation)
+		point_buff = np.linalg.norm([peripheral_filters.offsets_r[i][0][j],peripheral_filters.offsets_c[i][0][j]])
+		point_vector[j] = abs(point_buff)
+	dist_mask[i] = min(point_vector)
+	del point_vector
+
+# Compute Foveal Radius
+foveal_radius = np.ceil(np.mean(dist_mask))
+
+foveal_radius_px = foveal_radius
+foveal_radius_deg = foveal_radius * deg_per_pixel 
+
+foveal_mask = np.zeros((peri_height,peri_width))
+
+mid_point_w = np.round(peri_width/2)
+mid_point_h = np.round(peri_height/2)
+
+for i in range(0,peri_width):
+	for j in range(0,peri_height):
+		if np.sqrt(pow( i-mid_point_w,2) + pow(j-mid_point_h,2)) <= foveal_radius:
+			foveal_mask[j][i] = 1
+
+######################
+# Create Filter bank #
+######################
+
+# To do.
+# We have to add the gabor filter bank here.
+
+####################################
+# Create list of Foveal_parameters #
+####################################
+
+foveal_param = foveal_param_init_all()
+
+foveal_param.N_theta = N_theta
+foveal_param.N_e = N_e
 
